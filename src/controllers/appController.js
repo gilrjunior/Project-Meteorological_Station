41d5b41971004b77d8037ab896c53c28a406c0a8
gilrjunior <gilmarjunioreis@gmail.com>
2023-05-08 22:18:39 -0300

    Charts commit

--- src/controllers/appController.js
+++ src/controllers/appController.js
@@ -1,3 +1,6 @@
+var myLineChart = [];
+
+
 function loadFunction(){
 
     var ano = String(new Date().getFullYear());
@@ -6,46 +9,9 @@ function loadFunction(){
 
     var data = document.getElementById("data");
 
+    chart7days(true);
+
     data.innerText = `${dia}/${mes}/${ano}`
-
-}
-
-function change7(){
-
-    var g1 = document.getElementById("G1");
-    var g2 = document.getElementById("G2");
-    var g3 = document.getElementById("G3");
-
-    g1.src = "/img/graphic/dayHumidity7.png";
-    g2.src = "/img/graphic/dayRain7.png";
-    g3.src = "/img/graphic/dayTemp7.png";
-
-}
-
-function change30(){
-
-    var g1 = document.getElementById("G1");
-    var g2 = document.getElementById("G2");
-    var g3 = document.getElementById("G3");
-
-    g1.src = "/img/graphic/dayHumidity30.png";
-    g2.src = "/img/graphic/dayRain30.png";
-    g3.src = "/img/graphic/dayTemp30.png";
-
-
-}
-
-function changeY(){
-
-    var g1 = document.getElementById("G1");
-    var g2 = document.getElementById("G2");
-    var g3 = document.getElementById("G3");
-
-    g1.src = "/img/graphic/monthHumidity.png";
-    g2.src = "/img/graphic/monthRain.png";
-    g3.src = "/img/graphic/monthTemp.png";
-
-
 
 }
 
@@ -53,4 +19,97 @@ function refresh() {
 
     location.reload();
     
+}
+
+function standartChart(labels, data){
+
+    for(var i = 1; i < 4; i++){
+
+        var name = ""
+
+        if(i == 1){
+            name = "Temperatura °C"
+        }else if(i == 2){
+            name = "Umidade %"
+        }else{
+            name = "Preciptação"
+        }
+
+        const ctx = document.getElementById('ch'+i);
+
+        myLineChart[i-1] = new Chart(ctx, {
+            type: 'line',
+            data: {
+            labels: labels,
+            datasets: [{
+                label: name,
+                data: data,
+                borderWidth: 2,
+                borderColor: "#3B8952"
+            }]
+            },
+            options: {
+                scales: {
+                    y: {
+                    min: 16
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: name
+                    }
+                }
+            }
+        })
+
+
+    }
+
+}
+
+function chart7days(load){
+
+    if(!load){
+
+        deleteCharts();
+        
+    }
+
+    var labels = [String(new Date().getDate()-6).padStart(2, "0"), String(new Date().getDate()-5).padStart(2, "0"), String(new Date().getDate()-4).padStart(2, "0"), String(new Date().getDate()-3).padStart(2, "0"), String(new Date().getDate()-2).padStart(2, "0"), String(new Date().getDate()-1).padStart(2, "0"), String(new Date().getDate()).padStart(2, "0")]
+    var data = [28, 29, 24, 22, 19, 21, 23]
+    
+    standartChart(labels, data);
+
+}
+
+function chart30days(){
+
+    deleteCharts();
+
+    var labels = [01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+    var data = [25, 29, 24, 22, 19, 21, 23,20, 29, 19, 22, 19, 21, 18,28, 29, 27, 22, 19, 21, 23,21, 22, 24, 22, 19, 21, 23,28, 25, 24, 22, 19, 21, 23]
+
+    standartChart(labels, data);
+
+}
+
+function chart365days(){
+
+
+    deleteCharts();
+
+    var labels = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
+    var data = [25, 29, 24, 22, 19, 21, 23,20, 29, 19, 22, 19]
+
+    standartChart(labels, data);
+
+}
+
+function deleteCharts(){
+
+    myLineChart[0].destroy();
+    myLineChart[1].destroy();
+    myLineChart[2].destroy();
+
 }
